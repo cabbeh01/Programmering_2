e40968d4e8a5015c4ffb841663dc899aee1c6184
Casper Henningsson <cabbe.h01@gmail.com>
2019-09-10 14:39:15 +0200

    Klar uppgift 1_3 object

---
+++ 1_3-Object/1_3-Object/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_3_Object
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            PictureBox[,] bilder = new PictureBox[5, 5];
+
+            for(int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    bilder[r, c] = new PictureBox();
+                    bilder[r, c].BackgroundImage = Image.FromFile(@"C:\Users\cashen0116\Downloads\star.jpg");
+                    bilder[r, c].Left = (50*c) + 0;
+                    bilder[r, c].Width = 50;
+                    bilder[r, c].Height = 50;
+                    bilder[r, c].Top = (50*r) +0;
+                    bilder[r, c].Click += pbxStar_Click;
+                    this.Controls.Add(bilder[r,c]);
+                    
+                }
+            }
+
+
+        }
+
+        private void pbxStar_Click(object sender,EventArgs e)
+        {
+            PictureBox clickedflower = (PictureBox)sender;
+            if (clickedflower.BorderStyle == BorderStyle.FixedSingle)
+            {
+                clickedflower.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                clickedflower.BorderStyle = BorderStyle.FixedSingle;
+            }
+            
+
+            
+        }
+    }
+}
