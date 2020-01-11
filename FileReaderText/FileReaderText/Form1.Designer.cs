c314413565acee0abafd8786e934375dbc06c5fd
Cabbeh01 <cabbe.h01@gmail.com>
2020-01-11 16:01:52 +0100

    Klar!

---
+++ FileReaderText/FileReaderText/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FileReaderText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arkivToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nyttToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öppnaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sparaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sparaSomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stängToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tbxOrd1 = new System.Windows.Forms.TextBox();
+            this.tbxOrd2 = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.tbxOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dlgOpenFile
+            // 
+            this.dlgOpenFile.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arkivToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arkivToolStripMenuItem
+            // 
+            this.arkivToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nyttToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.öppnaToolStripMenuItem,
+            this.sparaToolStripMenuItem,
+            this.sparaSomToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.stängToolStripMenuItem});
+            this.arkivToolStripMenuItem.Name = "arkivToolStripMenuItem";
+            this.arkivToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.arkivToolStripMenuItem.Text = "Arkiv";
+            // 
+            // nyttToolStripMenuItem
+            // 
+            this.nyttToolStripMenuItem.Name = "nyttToolStripMenuItem";
+            this.nyttToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nyttToolStripMenuItem.Text = "Nytt";
+            this.nyttToolStripMenuItem.Click += new System.EventHandler(this.nyttToolStripMenuItem_Click);
+            // 
+            // öppnaToolStripMenuItem
+            // 
+            this.öppnaToolStripMenuItem.Name = "öppnaToolStripMenuItem";
+            this.öppnaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.öppnaToolStripMenuItem.Text = "Öppna";
+            this.öppnaToolStripMenuItem.Click += new System.EventHandler(this.öppnaToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sparaToolStripMenuItem
+            // 
+            this.sparaToolStripMenuItem.Name = "sparaToolStripMenuItem";
+            this.sparaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sparaToolStripMenuItem.Text = "Spara";
+            this.sparaToolStripMenuItem.Click += new System.EventHandler(this.sparaToolStripMenuItem_Click);
+            // 
+            // sparaSomToolStripMenuItem
+            // 
+            this.sparaSomToolStripMenuItem.Name = "sparaSomToolStripMenuItem";
+            this.sparaSomToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sparaSomToolStripMenuItem.Text = "Spara som...";
+            this.sparaSomToolStripMenuItem.Click += new System.EventHandler(this.sparaSomToolStripMenuItem_Click);
+            // 
+            // stängToolStripMenuItem
+            // 
+            this.stängToolStripMenuItem.Name = "stängToolStripMenuItem";
+            this.stängToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stängToolStripMenuItem.Text = "Stäng";
+            this.stängToolStripMenuItem.Click += new System.EventHandler(this.stängToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tbxOrd1
+            // 
+            this.tbxOrd1.Location = new System.Drawing.Point(484, 27);
+            this.tbxOrd1.Name = "tbxOrd1";
+            this.tbxOrd1.Size = new System.Drawing.Size(100, 20);
+            this.tbxOrd1.TabIndex = 2;
+            // 
+            // tbxOrd2
+            // 
+            this.tbxOrd2.Location = new System.Drawing.Point(607, 27);
+            this.tbxOrd2.Name = "tbxOrd2";
+            this.tbxOrd2.Size = new System.Drawing.Size(100, 20);
+            this.tbxOrd2.TabIndex = 3;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(713, 25);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 4;
+            this.btnChange.Text = "Ersätt";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // tbxOut
+            // 
+            this.tbxOut.Location = new System.Drawing.Point(12, 56);
+            this.tbxOut.Multiline = true;
+            this.tbxOut.Name = "tbxOut";
+            this.tbxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxOut.Size = new System.Drawing.Size(776, 382);
+            this.tbxOut.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxOut);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.tbxOrd2);
+            this.Controls.Add(this.tbxOrd1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arkivToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nyttToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem öppnaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sparaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sparaSomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem stängToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbxOrd1;
+        private System.Windows.Forms.TextBox tbxOrd2;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox tbxOut;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileReaderText/FileReaderText/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReaderText
+{
+    public partial class Form1 : Form
+    {
+
+        string Filename;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void stängToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void nyttToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filename = "";
+            tbxOut.Clear();
+        }
+
+        private void öppnaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult result = dlgOpenFile.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                Filename = dlgOpenFile.FileName;
+                FileStream inStream = new FileStream(Filename, FileMode.Open, FileAccess.Read);
+                
+
+                StreamReader reader = new StreamReader(inStream);
+                tbxOut.Clear();
+                string fileText = reader.ReadToEnd();
+
+                tbxOut.Text = fileText;
+
+                reader.Dispose();
+            }
+        }
+
+        private void sparaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Filename))
+            {
+                FileStream OutStream = new FileStream(Filename, FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(OutStream);
+                writer.Write(tbxOut.Text);
+                writer.Dispose();
+            }
+            else
+            {
+                DialogResult result = dlgSaveFile.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    FileStream OutStream = new FileStream(dlgSaveFile.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+
+                    StreamWriter writer = new StreamWriter(OutStream);
+                    writer.Write(tbxOut.Text);
+                    writer.Dispose();
+                }
+
+            }
+        }
+
+        private void sparaSomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgSaveFile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileStream OutStream = new FileStream(dlgSaveFile.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+
+                StreamWriter writer = new StreamWriter(OutStream);
+                writer.Write(tbxOut.Text);
+                writer.Dispose();
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            tbxOut.Text = tbxOut.Text.Replace(tbxOrd1.Text, tbxOrd2.Text);
+        }
+    }
+}
