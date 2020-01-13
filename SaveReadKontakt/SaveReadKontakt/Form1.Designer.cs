4f33a28095aabccef516dfc9e41cba1ed2b73761
Casper Henningsson <cabbe.h01@gmail.com>
2020-01-13 09:05:08 +0100

    Klar!

---
+++ SaveReadKontakt/SaveReadKontakt/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SaveReadKontakt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxFirstname = new System.Windows.Forms.TextBox();
+            this.tbxLastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPhonenumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Förnamn";
+            // 
+            // tbxFirstname
+            // 
+            this.tbxFirstname.Location = new System.Drawing.Point(102, 47);
+            this.tbxFirstname.Name = "tbxFirstname";
+            this.tbxFirstname.Size = new System.Drawing.Size(138, 20);
+            this.tbxFirstname.TabIndex = 1;
+            // 
+            // tbxLastname
+            // 
+            this.tbxLastname.Location = new System.Drawing.Point(102, 73);
+            this.tbxLastname.Name = "tbxLastname";
+            this.tbxLastname.Size = new System.Drawing.Size(138, 20);
+            this.tbxLastname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Efternamn";
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Location = new System.Drawing.Point(102, 99);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(138, 20);
+            this.tbxEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "E-post";
+            // 
+            // tbxPhonenumber
+            // 
+            this.tbxPhonenumber.Location = new System.Drawing.Point(102, 125);
+            this.tbxPhonenumber.Name = "tbxPhonenumber";
+            this.tbxPhonenumber.Size = new System.Drawing.Size(138, 20);
+            this.tbxPhonenumber.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefonnummer";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(258, 100);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Öppna";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(258, 126);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveAs.TabIndex = 9;
+            this.btnSaveAs.Text = "Spara som";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.BtnSaveAs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 159);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.tbxPhonenumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxLastname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxFirstname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxFirstname;
+        private System.Windows.Forms.TextBox tbxLastname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPhonenumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+    }
+}
+
---
+++ SaveReadKontakt/SaveReadKontakt/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveReadKontakt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeExtenstions();
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgOpenFile.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                FileStream inStream = new FileStream(dlgOpenFile.FileName, FileMode.Open, FileAccess.Read);
+
+                StreamReader stream = new StreamReader(inStream);
+                string firstname = stream.ReadLine();
+                string lastname = stream.ReadLine();
+                string email = stream.ReadLine();
+                string phonenumber = stream.ReadLine();
+                tbxFirstname.Text = firstname;
+                tbxLastname.Text = lastname;
+                tbxEmail.Text = email;
+                tbxPhonenumber.Text = phonenumber;
+                stream.Dispose();
+            }
+        }
+
+        private void BtnSaveAs_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgSaveFile.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FileStream outStream = new FileStream(dlgSaveFile.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                
+                StreamWriter stream = new StreamWriter(outStream);
+                string firstname = tbxFirstname.Text;
+                string lastname = tbxLastname.Text;
+                string email = tbxEmail.Text;
+                string phonenumber = tbxPhonenumber.Text;
+
+                stream.WriteLine(firstname);
+                stream.WriteLine(lastname);
+                stream.WriteLine(email);
+                stream.WriteLine(phonenumber);
+
+                stream.Dispose();
+            }
+        }
+
+        void InitializeExtenstions()
+        {
+            dlgSaveFile.Filter = "Kontakt filer (*.kon*)|*.kon*";
+            dlgSaveFile.DefaultExt = "kon";
+            dlgSaveFile.AddExtension = true;
+
+            dlgOpenFile.Filter = "Kontakt filer (*.kon*)|*.kon*";
+            dlgOpenFile.DefaultExt = "kon";
+            dlgOpenFile.AddExtension = true;
+        }
+
+        void ClearTextboxes()
+        {
+            tbxFirstname.Clear();
+            tbxLastname.Clear();
+            tbxEmail.Clear();
+            tbxPhonenumber.Clear();
+        }
+    }
+}
